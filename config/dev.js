c4e5d06ed84af44b969e36c1d6d5520c65f0036e
Yehia Saleh <yehia.saleh@yale.edu>
2018-03-02 22:05:30 -0500

    New structure

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+/*
+ * Contains all the development keys -- do not commit this
+ */
+module.exports = {
+  googleClientID: '351204203605-2odbmhjodlp797iv2u7jb9vdesqpl5e7.apps.googleusercontent.com',
+  googleClientSecret: 'RUZjqgLwlFh4JBNYtGBulPcc',
+  mongoURI: 'mongodb://yehia:password@ds249398.mlab.com:49398/emaily-dev',
+  cookieKey: 'hajlsdhflkasdhkjlfhwqetqgrsb'
+};
---
+++ config/prod.js
@@ -0,0 +1,9 @@
+/*
+ * Contains all production keys -- commit this
+ */
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
