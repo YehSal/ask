fa536712de43040e89640a665ebfc42d25ae764d
Yehia Saleh <yehia.saleh@yale.edu>
2018-03-02 22:07:48 -0500

    delete old server directory

--- server/config/keys.js
+++
@@ -1,8 +0,0 @@
-/*
- * Figures out which credentials to use based on dev/prod environment
- */
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./prod');
-} else {
-  module.exports = require('./dev');
-}
--- server/config/prod.js
+++
@@ -1,9 +0,0 @@
-/*
- * Contains all production keys -- commit this
- */
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
-};
