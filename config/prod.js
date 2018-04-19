57befb1ef8545a83b8fcebe03191dbb90973aa89
Yehia Saleh <yehia.saleh@yale.edu>
2018-04-19 00:10:25 -0400

    Deployment patch

--- config/prod.js
+++ config/prod.js
@@ -5,7 +5,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
   nodemailerUser: process.env.NODEMAILER_USER,
   nodemailerPass: process.env.NODEMAILER_PASS
 };
