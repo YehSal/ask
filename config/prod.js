1c550f8b494e161d5659d7ede78879d31e66c7a3
Yehia Saleh <yehia.saleh@yale.edu>
2018-04-19 00:06:33 -0400

    Heroku deployment

--- config/prod.js
+++ config/prod.js
@@ -6,4 +6,6 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY
+  nodemailerUser: process.env.NODEMAILER_USER,
+  nodemailerPass: process.env.NODEMAILER_PASS
 };
