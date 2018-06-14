7317d1393c20d82c2b7921f4920c92ea426fca78
Yehia Saleh <yehia.saleh@yale.edu>
2018-06-14 11:15:32 -0400

    Landing Scss refactoring

--- client/src/helpers/userHelpers.js
+++ client/src/helpers/userHelpers.js
@@ -1,3 +1,6 @@
+/*
+ * Helper functions that fetches information about the current user
+ */
 export const isStudent = (user) => user && user.role === 2 ? true : false;
 export const isProfessor = (user) => user && user.role === 1 ? true: false;
 export const isUnregistered = (user) => user && user.role == 0 ? true: false;
